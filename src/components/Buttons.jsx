7163a763213f2f5e8cc95adbd11fe825dd78c81c
dineshraj100 <dineshrajmech01@gmail.com>
2024-12-02 17:49:37 +0530

    povi branch created

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -7,6 +7,7 @@ const Buttons = ({ onAdd, onRemove, onAddAll, onRemoveAll }) => {
       <button onClick={onAdd}>Add</button>
       <button onClick={onRemove}>Remove</button>
       <button onClick={onAddAll}>Add All</button>
+      <button onClick={onAddAll}>Add All</button>
       <button onClick={onRemoveAll}>Remove All</button>
     </div>
   );
